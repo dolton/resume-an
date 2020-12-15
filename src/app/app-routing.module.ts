b0efff64fb5cb841cc8cc66f1801ddbecc54ec21
nqe00225 <>
2020-12-15 14:57:28 +0530

    adding new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './web-components/home/home.component';
+import { ContactUsComponent } from './web-components/contact-us/contact-us.component';
+import { ResumeComponent } from './web-components/resume/resume.component';
+import { PortfolioComponent } from './web-components/portfolio/portfolio.component';
+import { BlogComponent } from './web-components/blog/blog.component';
+import { AboutComponent } from './web-components/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { AppHeaderComponent } from './app-layout/app-header/app-header.component';
+import { AppBodyComponent } from './app-layout/app-body/app-body.component';
+import { AppFooterComponent } from './app-layout/app-footer/app-footer.component';
+import { NavbarComponent } from './app-layout/app-header/navbar/navbar.component';
+import { HomeComponent } from './web-components/home/home.component';
+import { PortfolioComponent } from './web-components/portfolio/portfolio.component';
+import { ResumeComponent } from './web-components/resume/resume.component';
+import { AboutComponent } from './web-components/about/about.component';
+import { BlogComponent } from './web-components/blog/blog.component';
+import { ContactUsComponent } from './web-components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    AppHeaderComponent,
+    AppBodyComponent,
+    AppFooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
